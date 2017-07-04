5c85635c99ca5c50d39f8af1cadd6699ae6300df
borenho <borecollins@gmail.com>
2017-07-04 16:17:15 +0300

    Draw square, circle and triangle

--- mindstorms.py
+++ mindstorms.py
@@ -1,30 +1,48 @@
 import turtle     # Module used to do the drawing
 
-def lets_draw():
+def draw_square(some_turtle):
+	for i in range(4):
+		some_turtle.forward(150)    # Move him forwad 100 steps
+		some_turtle.right(90)    # Turn right 90 degrees
+
+def draw_triangle(some_turtle):
+	for i in range(3):
+		some_turtle.backward(200)    # Move him backwards 100 steps
+		some_turtle.left(120)    # Turn right 45 degrees
+
+def draw_art():
 	# Create canvas to draw the square on
 	window = turtle.Screen()
 	window.bgcolor("pink")
 
-	# Make the drawing
+	# Create turtle brad to draw square
 	brad = turtle.Turtle()    # Instantiate brad
 	# Customize brad
 	brad.shape("turtle")
 	brad.color("green")
-	brad.speed(2)
+	brad.speed(3)
+	# Call brad
+	draw_square(brad)
 
-	brad.forward(100)    # Move him forwad 100 steps
-	brad.right(90)    # Turn right 90 degrees
+	# Create turtle angie to draw circle
+	angie = turtle.Turtle()    # Instantiate brad
+	# Customize angie
+	angie.shape("arrow")
+	angie.color("grey")
+	angie.speed(5)
+	angie.circle(100)
 
-	brad.forward(100)    # Move him forwad 100 steps
-	brad.right(90)    # Turn right 90 degrees
-
-	brad.forward(100)    # Move him forwad 100 steps
-	brad.right(90)    # Turn right 90 degrees
-
-	brad.forward(100)    # Move him forwad 100 steps
-	brad.right(90)    # Turn right 90 degrees
+	# Create turtle croco to draw triangle
+	croco = turtle.Turtle()    # Instantiate croco
+	# Customize croco
+	croco.shape("arrow")
+	croco.color("blue")
+	croco.speed(3)
+	# Call croco
+	draw_triangle(croco)
+	
 
 	window.exitonclick()
 
 # Make sure to call the function
-lets_draw()
+draw_art()
